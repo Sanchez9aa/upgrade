f709f5fb7b8f2d910107970ec8af90efb58c55b3
Sanchez9aa <sanchez.moral.jesus@gmail.com>
2023-10-30 23:15:20 +0100

    feat: finishing tareas-web

--- html-js-css/junior/tareas-web/main.js
+++ html-js-css/junior/tareas-web/main.js
@@ -1,21 +1,58 @@
 let tasks = [];
 
+if (localStorage.getItem("tasks")) {
+	tasks = JSON.parse(localStorage.getItem("tasks"));
+	if (tasks.length > 0) {
+		displayTasks();
+	}
+}
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 
+function checkTask(taskValue) {
+	if (taskValue === "") {
+		alert("Debes escribir una tarea");
+		return false;
+	}
+	if (tasks.some((task) => task === taskValue)) {
+		alert("La tarea ya existe");
+		return false;
+	}
+	return true;
+}
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+	if (!checkTask(input.value)) {
+		return;
+	}
 	addTask(input.value);
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 	input.value = "";
 });
 
 function addTask(value) {
-	tasks.push(input.value);
+	tasks.push(value);
 
-	displayTasks();
+	displayTask(value);
 }
 
-function displayTasks() {
+function deleteTask(li) {
+	const index = tasks.indexOf(li.innerText);
+	tasks.splice(index, 1);
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	if (tasks.length === 0) {
+		localStorage.removeItem("tasks");
+	}
+	li.remove();
+
+	if (tasks.length === 0) {
+		document.querySelector(".tareas-container").remove();
+	}
+}
+
+function createTasksStructure() {
 	const main = document.querySelector("main");
 
 	const section = document.createElement("section");
@@ -28,19 +65,46 @@ function displayTasks() {
 	ul.addEventListener("click", (event) => {
 		tacharTask(event.target);
 	});
+	ul.addEventListener("dblclick", (event) => {
+		deleteTask(event.target);
+	});
 
 	section.appendChild(h2);
 	section.appendChild(ul);
+
 	main.appendChild(section);
+}
+
+function displayTasks() {
+	if (!document.querySelector(".tareas-container")) {
+		createTasksStructure();
+	}
+
+	const ul = document.querySelector("ul");
 
 	tasks.forEach((task) => {
-		const li = `<li>${task}</li>`;
-		ul.innerHTML += li;
+		const li = document.createElement("li");
+		li.innerText = task;
+
+		ul.appendChild(li);
 	});
 }
 
+function displayTask(taskValue) {
+	if (!document.querySelector(".tareas-container")) {
+		console.log("hola");
+		createTasksStructure();
+	}
+
+	const ul = document.querySelector("ul");
+
+	const li = document.createElement("li");
+	li.innerText = taskValue;
+
+	ul.appendChild(li);
+}
+
 function tacharTask(li) {
-	console.log(li);
 	if (li.classList.contains("tachado")) {
 		li.classList.remove("tachado");
 	} else {
